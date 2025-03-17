c63aaba5d2d7e5fc694901db3d15001c3a97424c
Ydavies <hiro.davies@gmail.com>
2025-03-17 14:26:34 +1100

    17.3.2025 working in class

---
+++ part2.js
@@ -0,0 +1,26 @@
+// We are setting the string values
+let myString = "this is a string!";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "My name is Hiro";
+
+console.log(myString);
+console.log(anotherString);
+console.log(hello);
+console.log(myName);
+
+// Here we are using the length property to display the string length
+let length = myString.length;
+console.log(length);
+
+// Using the charAt() method to display the first character in the string
+let char0 = myString.charAt(0);
+console.log(char0);
+
+// using same method as above but changing the value we are searching for
+let char11 = myString.charAt(10);
+console.log(char11);
+
+// using slice to get is a from myString
+let slice1 = myString.slice(5, 9);
+console.log(slice1);
