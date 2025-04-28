4a1f9a8efbdb6c2bd38bb3a90bcb9616a438edc1
Ydavies <hiro.davies@gmail.com>
2025-04-28 16:12:20 +1000

    finished search array

--- part4.js
+++ part4.js
@@ -21,15 +21,41 @@ const movieList = [
   { movieId: "8", title: "Interstellar", year: "2014", rating: "8.7" },
 ];
 
-
-
 // const movieSorted = movieList.sort((a, b) => a.movieID - b.movieID);
 // console.log(movieSorted);
 
 // After not being able to get the array to sort i asked chatGTP and found that it was sorting it by a string and not a number, so i changed it to parseInt
 
-movieList.sort((a,b) => parseInt(a.movieId) - parseInt(b.movieId));
+movieList.sort((a, b) => parseInt(a.movieId) - parseInt(b.movieId));
 console.log(movieList);
 
+// What are we searching for?
+let Result = "21";
+// creating a search function
+function movieIdsearch(movieList, movieId) {
+  // if not found then we set the value to -1
+  let found = -1;
+  // console.log(`Array: ${movieList}`);
+  console.log(`Searching for: Movie ${movieId}`);
+  //Search the array by creating a loop
+  for (let i = 0; i < movieList.length; i++) {
 
+    // we can uncomment the below code if we want to show a console log of each search process that is completed.
+    // console.log(`movieList[${i}]: ${movieList[i].movieId}`);
 
+    if (movieList[i].movieId === movieId) {
+      found = i;
+      // end the loop
+      break;
+    }
+  }
+  return found;
+}
+// Calling the function
+const result = movieIdsearch(movieList, Result);
+// Output of result
+if (result == -1) {
+  console.log(`Result: nul`);
+} else {
+  console.log(`The Movie of ${Result} was found at index ${result}`);
+}
